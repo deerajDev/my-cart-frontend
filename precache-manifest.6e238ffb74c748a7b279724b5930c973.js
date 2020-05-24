bfc6c738e10bac56f45aafab739535725cc78b89
deeraj <deerajn04@gmail.com>
2020-05-24 17:44:28 +0530

    Updates

---
+++ precache-manifest.6e238ffb74c748a7b279724b5930c973.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43dc310086b25fea1880202a725f8e76",
+    "url": "/my-cart-frontend/index.html"
+  },
+  {
+    "revision": "56bd5b02018d74b93094",
+    "url": "/my-cart-frontend/static/css/main.352e3dad.chunk.css"
+  },
+  {
+    "revision": "da7682f1f7c354369b3c",
+    "url": "/my-cart-frontend/static/js/2.1136bab8.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/my-cart-frontend/static/js/2.1136bab8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56bd5b02018d74b93094",
+    "url": "/my-cart-frontend/static/js/main.23ad63f8.chunk.js"
+  },
+  {
+    "revision": "dfdd91620c40a80f578b",
+    "url": "/my-cart-frontend/static/js/runtime-main.2f0b4295.js"
+  },
+  {
+    "revision": "35f0b6b351be259aa5358c310161a489",
+    "url": "/my-cart-frontend/static/media/logo.35f0b6b3.svg"
+  }
+]);
